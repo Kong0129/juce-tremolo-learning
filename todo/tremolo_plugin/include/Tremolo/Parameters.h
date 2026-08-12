4c29877f007100fd37cd74cef0be0d268ce5c9a0
Kong0129 <2773737419@qq.com>
2026-08-12 12:38:50 +0800

    12: register waveform parameter

--- todo/tremolo_plugin/include/Tremolo/Parameters.h
+++ todo/tremolo_plugin/include/Tremolo/Parameters.h
@@ -3,5 +3,10 @@
 namespace tremolo {
 struct Parameters {
   explicit Parameters(juce::AudioProcessor&);
+
+  juce::AudioParameterChoice& waveform;
+
+  JUCE_DECLARE_NON_COPYABLE(Parameters)
+  JUCE_DECLARE_NON_MOVEABLE(Parameters)
 };
 }  // namespace tremolo
