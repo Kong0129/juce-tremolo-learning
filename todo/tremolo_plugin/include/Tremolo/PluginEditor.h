69bb8dbd90c24838e03edc82e3cb82d757117e55
Kong0129 <2773737419@qq.com>
2026-08-22 12:22:13 +0800

    28: add labels to plugin controls

--- todo/tremolo_plugin/include/Tremolo/PluginEditor.h
+++ todo/tremolo_plugin/include/Tremolo/PluginEditor.h
@@ -10,14 +10,18 @@ public:
 private:
   juce::ImageComponent background;
   juce::ImageComponent logo;
+  juce::Label waveformLabel;
   juce::ComboBox waveformSelector;
   juce::ComboBoxParameterAttachment waveformAttachment;
+  juce::Label rateLabel;
   juce::Slider rateSlider;
   juce::SliderParameterAttachment rateAttachment;
+  juce::Label depthLabel;
   juce::Slider depthSlider;
   juce::SliderParameterAttachment depthAttachment;
   juce::ToggleButton bypassButton;
   juce::ButtonParameterAttachment bypassButtonAttachment;
+
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginEditor)
 };
 }  // namespace tremolo
--- todo/tremolo_plugin/source/PluginEditor.cpp
+++ todo/tremolo_plugin/source/PluginEditor.cpp
@@ -5,6 +5,9 @@ PluginEditor::PluginEditor(PluginProcessor& p)
       rateAttachment(p.getRateParameter(), rateSlider, nullptr),
       depthAttachment(p.getDepthParameter(), depthSlider, nullptr),
       bypassButtonAttachment(p.getBypassedParameter(), bypassButton, nullptr) {
+  waveformLabel.setText("Waveform", juce::dontSendNotification);
+  rateLabel.setText("Rate", juce::dontSendNotification);
+  depthLabel.setText("Depth", juce::dontSendNotification);
   background.setImage(juce::ImageCache::getFromMemory(
       assets::Background_png, assets::Background_pngSize));
 
@@ -16,6 +19,10 @@ PluginEditor::PluginEditor(PluginProcessor& p)
   waveformSelector.addItem("Sine", 1);
   waveformSelector.addItem("Triangle", 2);
   waveformAttachment.sendInitialUpdate();
+  waveformLabel.setJustificationType(juce::Justification::centred);
+  rateLabel.setJustificationType(juce::Justification::centred);
+  depthLabel.setJustificationType(juce::Justification::centred);
+  addAndMakeVisible(waveformLabel);
   addAndMakeVisible(waveformSelector);
   rateSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
   rateSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 80, 20);
@@ -24,9 +31,12 @@ PluginEditor::PluginEditor(PluginProcessor& p)
   depthSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 80, 20);
   depthSlider.setTextValueSuffix(" %");
   addAndMakeVisible(rateSlider);
+  addAndMakeVisible(rateLabel);
+  addAndMakeVisible(depthLabel);
   addAndMakeVisible(depthSlider);
   bypassButton.setButtonText("Bypass");
   addAndMakeVisible(bypassButton);
+
   // Make sure that before the constructor has finished, you've set the
   // editor's size to whatever you need it to be.
   setSize(540, 270);
@@ -46,5 +56,9 @@ void PluginEditor::resized() {
   depthSlider.setBounds(320, 48, 100, 100);
 
   bypassButton.setBounds(440, 64, 80, 32);
+
+  waveformLabel.setBounds(16, 40, 160, 24);
+  rateLabel.setBounds(200, 24, 100, 24);
+  depthLabel.setBounds(320, 24, 100, 24);
 }
 }  // namespace tremolo
