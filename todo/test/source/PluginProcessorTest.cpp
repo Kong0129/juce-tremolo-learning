45ace8c67be2b78addadf736136dcd100e7dc3f5
Kong0129 <2773737419@qq.com>
2026-09-07 18:57:54 +0800

    34: test plugin state round trip

--- todo/test/source/PluginProcessorTest.cpp
+++ todo/test/source/PluginProcessorTest.cpp
@@ -2,9 +2,29 @@
 #include <gtest/gtest.h>
 
 namespace tremolo {
-/** This test doesn't test anything but shows that you can instantiate and use a
- * plugin processor in test */
-TEST(PluginProcessor, PlaceholderTest) {
-  PluginProcessor processor{};
+
+TEST(PluginProcessor, StateRoundTripRestoresParameters) {
+  PluginProcessor sourceProcessor{};
+
+  sourceProcessor.getWaveformParameter() = 1;
+  sourceProcessor.getRateParameter() = 7.25f;
+  sourceProcessor.getDepthParameter() = 63.3f;
+  sourceProcessor.getBypassedParameter() = true;  // Arrange
+
+  juce::MemoryBlock stateData;
+  sourceProcessor.getStateInformation(stateData);
+
+  ASSERT_GT(stateData.getSize(), 0u);
+
+  PluginProcessor restoredProcessor{};
+  restoredProcessor.setStateInformation(stateData.getData(),
+                                        static_cast<int>(stateData.getSize()));  //Act
+
+  EXPECT_EQ(restoredProcessor.getWaveformParameter().getIndex(), 1);
+
+  EXPECT_NEAR(restoredProcessor.getRateParameter().get(), 7.25f, 1.0e-6f);
+  EXPECT_NEAR(restoredProcessor.getDepthParameter().get(), 63.3f, 1.0e-6f);
+
+  EXPECT_TRUE(restoredProcessor.getBypassedParameter().get());
 }
 }  // namespace tremolo
